d28667105335883491d4e07ee3dc3b037b3ed08f
sinnerman <sinnerman@gmail.com>
2022-09-10 21:33:19 +0400

    initial

---
+++ pages/about.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, et
+          minus. Similique ratione magni quisquam consequatur dignissimos
+          sapiente cum nulla odit beatae ipsam, cupiditate voluptates,
+          laboriosam ad. Sequi, consequuntur adipisci?
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, et
+          minus. Similique ratione magni quisquam consequatur dignissimos
+          sapiente cum nulla odit beatae ipsam, cupiditate voluptates,
+          laboriosam ad. Sequi, consequuntur adipisci?
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
